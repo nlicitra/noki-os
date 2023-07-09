2e24789a664176437bd788bb981ed2508e87475b
nlicitra <nickerous@gmail.com>
2023-07-08 22:46:57 -0400

    tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -3,31 +3,23 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(noki_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-#![allow(dead_code)]
-
 
 use core::panic::PanicInfo;
 use noki_os::println;
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
     loop {}
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     noki_os::test_panic_handler(info);
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello world{}", "!");
-
-    #[cfg(test)]
-    test_main();
-
-    loop {}
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
